8cc26d5f3d437cb50d63d1f8d43b44ba5201fd31
Zintaho <zintaho@gmail.com>
2022-07-30 19:10:40 +0900

    엄청난 부자2

---
+++ nodejs/bronze5/1271.js
@@ -0,0 +1,7 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+input = input.map(x => BigInt(x));
+let n = input[0];
+let m = input[1];
+console.log((n/m).toString());
+console.log((n%m).toString());
