ed808609b437252941a906fa53ae6a353113f91d
Zintaho <zintaho@gmail.com>
2022-07-30 15:17:24 +0900

    사칙연산

--- nodejs/newbie/10998.js
+++ nodejs/newbie/10998.js
@@ -1,4 +1,6 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
 input = input.map((x) => parseInt(x));
-console.log(input[0] * input[1]);
+let a = input[0];
+let b = input[1];
+console.log(a+b, a-b, a*b, Math.floor(a/b), a%b);
