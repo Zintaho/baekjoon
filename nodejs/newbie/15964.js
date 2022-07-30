b8243b539a4960e7886614825b07601d749ba7eb
Zintaho <zintaho@gmail.com>
2022-07-30 18:48:18 +0900

    이상한 기호

---
+++ nodejs/newbie/15964.js
@@ -0,0 +1,6 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+input = input.map(x => parseInt(x));
+let a = input[0];
+let b = input[1];
+console.log((a+b)*(a-b));
