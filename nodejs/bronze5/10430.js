80f7780ec46d9d60d0186da11e85296bd44a0142
Zintaho <zintaho@gmail.com>
2022-08-01 00:45:02 +0900

    나머지

---
+++ nodejs/bronze5/10430.js
@@ -0,0 +1,10 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+input = input.map(x => parseInt(x));
+let A = input[0];
+let B = input[1];
+let C = input[2];
+console.log((A+B)%C);
+console.log(((A%C) + (B%C))%C);
+console.log((A*B)%C);
+console.log(((A%C) * (B%C))%C);
