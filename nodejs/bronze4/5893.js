3e5a71be89d0bcb0b4772627cb8542601bab71ad
Zintaho <zintaho@gmail.com>
2022-08-09 22:21:06 +0900

    17배

--- nodejs/bronze4/5893.js
+++ nodejs/bronze4/5893.js
@@ -1,11 +1,4 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim();
-let len = input.length;
-let k = BigInt(1);
-let N = BigInt(0);
-for (i = len - 1; i >= 0 ; --i) {
-    if(parseInt(input[i])) N += k;
-    k *= BigInt(2);
-}
-N *= BigInt(17);
+let N = BigInt(`0b${input}`) * 17n;
 console.log(N.toString(2));
