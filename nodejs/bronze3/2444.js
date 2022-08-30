b14a16a8bf7c2470282c46fe8feb43e9b3c30660
Zintaho <zintaho@gmail.com>
2022-08-30 23:17:53 +0900

    별 찍기 - 7

---
+++ nodejs/bronze3/2444.js
@@ -0,0 +1,13 @@
+let N = parseInt(require('fs').readFileSync('/dev/stdin').toString().trim(), 10);
+let a = new Array(N-1).fill(' ');
+let b = new Array(2*N - 1).fill('*');
+let output = ''
+for (i = 1; i < N; ++i) {
+    output += a.slice(0, N-i).join('');
+    output += `${b.slice(0,2*i - 1).join('')}\n`;
+}
+for (i = N; i >= 1; --i) {
+    output += a.slice(0, N-i).join('');
+    output += `${b.slice(0,2*i - 1).join('')}\n`;
+}
+console.log(output);
