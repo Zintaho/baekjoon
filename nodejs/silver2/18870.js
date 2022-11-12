85ac24bba4125a50d9d2d247b9fff60eff7e635d
Zintaho <zintaho@gmail.com>
2022-11-12 15:09:23 +0900

    좌표 압축

--- nodejs/silver2/18870.js
+++ nodejs/silver2/18870.js
@@ -1,9 +1,16 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 let N = parseInt(input[0], 10);
 let nums = input[1].split(' ').slice(0,N).map(x=>parseInt(x, 10));
-let map = {};
+let sorted = [...new Set(nums)].sort((a,b) => a-b);
 
-[...new Set(nums)].sort((a,b) => a-b).forEach((v,i) => map[v]=i);
+let bSearch = (arr, v, start=0, end=sorted.length-1) => {
+    let mid = start + Math.floor((end-start)/2);
+    let a = arr[mid];
+    if (a === v) return mid;
+    else if (a < v) return bSearch(arr, v, mid+1, end);
+    else if (a > v) return bSearch(arr, v, start, mid-1);
+}
 
-nums = nums.map(x=>map[x]).join(' ');
-console.log(nums);
+nums = nums.map(x=>bSearch(sorted, x));
+
+console.log(nums.join(' '));
