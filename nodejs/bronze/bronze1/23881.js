598e1661da96c66286f14e581964d31208334d29
Zintaho <zintaho@gmail.com>
2023-01-26 23:24:01 +0900

    알고리즘 수업 - 선택 정렬 1

---
+++ nodejs/bronze/bronze1/23881.js
@@ -0,0 +1,20 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let [N, K] = input[0].split(' ').map(x=>+x);
+let A = input[1].split(' ').map(x=>+x);
+let swapped = [];
+const selection_sort = (A) => {
+    for (let last = N-1; last >= 0; --last) {
+        let [max, I] = [0, 0];
+        for (let i = 0; i <= last; ++i) {
+            if (A[i] > max) [max, I] = [A[i], i];
+        }
+
+        if (last != I) {
+            let [a,b] = [A[I], A[last]];
+            [A[last], A[I]] = [a,b];
+            swapped.push([Math.min(a,b), Math.max(a,b)]);
+        };
+    }
+}
+selection_sort(A);
+console.log(K>swapped.length? -1 : swapped[K-1].join(' '));
