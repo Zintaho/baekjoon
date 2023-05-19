9a40f764bdd7cbc3e1c24027e482179f0b67b297
Zintaho <zintaho@gmail.com>
2023-05-19 12:44:41 +0900

    N과 M (8)

---
+++ nodejs/silver/silver3/15657.js
@@ -0,0 +1,21 @@
+let [NM, seq]= require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let [N, M] = NM.split(' ').map(x=>+x);
+seq = seq.split(' ').map(x=>+x);
+seq.sort((a,b ) => a-b);
+let output = [];
+all_permutation(N, M);
+console.log(output.join('\n'));
+
+function all_permutation(n, r, row=[]) {
+    const len = row.length;
+    if (len > 1 && row[len - 2] > row[len - 1] ) return;
+    if (r === len) {
+        output.push(row.join(' '));
+        return;
+    }
+    for (let i = 0; i < n; ++i) {
+        let copied = [...row, seq[i]];
+
+        all_permutation(n, r, copied);
+    }
+}
