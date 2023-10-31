3d53d5ad7b57a82ab15b951ed82cdf40193a2835
Zintaho <zintaho@gmail.com>
2023-10-31 20:37:10 +0900

    부분수열의 합

---
+++ nodejs/silver/silver2/1182.js
@@ -0,0 +1,15 @@
+let [NS, input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+let [N, S] = NS.split(' ').map(Number);
+input = input.split(' ').map(Number);
+input.sort((a,b) => a-b);
+
+let output = 0;
+dfs();
+console.log(output);
+function dfs(i = 0, sum = 0, cnt=0) {
+    if (i === N && sum === S && cnt > 0) {output ++; return;}
+    else if (i === N) return;
+    
+    dfs(i+1, sum, cnt);
+    dfs(i+1, sum+input[i], cnt+1);
+}
