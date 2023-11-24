7c6bc48501e66b9ce13291962d8f5a20d9aeec6e
Zintaho <zintaho@gmail.com>
2023-11-24 22:33:19 +0900

    친구

--- nodejs/silver/silver2/1058.js
+++ nodejs/silver/silver2/1058.js
@@ -1,7 +1,7 @@
 let [N, ...matrix] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 N= +N;
 matrix.forEach((x,i) => {
-    matrix[i] = x.split('').map((y)=>{
+    matrix[i] = x.split('').map(y=>{
         switch(y) {
             case 'N':
                 return 0;
@@ -11,19 +11,22 @@ matrix.forEach((x,i) => {
     })
 });
 
-let friends = Array.from({length:N}, _=>new Array(N).fill(0))
-for (let m = 0; m < N; ++m) {
-    for (let y = 0; y < N; ++y) {
-        for (let x = 0; x < N; ++x) {
-            if (y===x) continue;
-            if (matrix[y][x] === 1 || (matrix[y][m] + matrix[m][x]) === 2) friends[y][x] = 1;
+let newMatrix = Array.from({length:N}, _=>new Array(N).fill(0));
+for (let y = 0; y < N; ++y) {
+    for (let x = 0; x < N; ++x) {
+        for (let i = 0; i < N; ++i) {
+            newMatrix[y][x] += matrix[y][i] * matrix[i][x];
         }
     }
 }
 
 let max = 0;
-friends.forEach(x=>{
-    max = Math.max(max, x.reduce((a,b) => a+b))
-});
+for (let y = 0; y < N; ++y) {
+    let sum = 0;
+    for (let x = 0; x< N; ++x) {
+        if (y===x) continue;
+        if (matrix[y][x] !== 0 || newMatrix[y][x] !== 0) sum++;
+    }
+    max = Math.max(sum, max);
+}
 console.log(max);
-
