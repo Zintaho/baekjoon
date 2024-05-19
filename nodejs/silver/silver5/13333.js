932d8e3a34f901b5d03a4b2414e50e8a34260991
Zintaho <zintaho@gmail.com>
2024-05-19 17:39:23 +0900

    13333 갱신

--- nodejs/silver/silver5/13333.js
+++ nodejs/silver/silver5/13333.js
@@ -1,22 +1,14 @@
 let [N, input] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 N = +N;
 input = input.split(' ').map(Number);
-input.sort((a,b ) => a-b);
-
-for (let k = 0; k <= N; ++k) {
-    let lCount = 0;
-    let sameCount = 0;
-    for (let a of input) {
-        if (a <= k) lCount++;
-        if (a === k) sameCount++;
-        
-        if (a > k) break;
+input.sort((a,b ) => b-a);
+getResult();
+function getResult() {
+for (let k = 1; k <= N; ++k) {
+    if (input[k-1] < k) {
+        console.log(k-1);
+        return;
     }
-
-    for (let j = lCount - sameCount; j <= lCount; ++j) {
-        if ((N- k) === j) {
-            console.log(k);
-            break;
-        }
-    }
+}
+console.log(N);
 }
